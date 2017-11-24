1491f063be0c510c5bf62ec5d78e2318910c12fa
Abhimanyu Singh Gaur <abhimanyusinghgaur@gmail.com>
2017-11-25 04:30:31 +0530

    Optimized Function

--- getKeyFrames.m
+++ getKeyFrames.m
@@ -8,24 +8,24 @@ function [ keyFrames ] = getKeyFrames( videoURI, requiredFps )
     video = VideoReader(videoURI);
     totalFrames = video.NumberOfFrames;
     requiredNumberOfFrames = uint32(video.Duration * requiredFps);
+    
+    if totalFrames <= requiredNumberOfFrames
+        requiredNumberOfFrames = totalFrames;
+    end
+    
+    keyFrames = zeros(video.Height, video.Width, video.BitsPerPixel/8, requiredNumberOfFrames, 'uint8');
+    frameInterval = ( totalFrames - rem(totalFrames, requiredNumberOfFrames) ) / requiredNumberOfFrames;
 
 %     re-initialize because of the problem with video.NumberOfFrames
     video = VideoReader(videoURI);
-    
-    if totalFrames <= requiredNumberOfFrames
-        keyFrames = read(video);
-    else
-        keyFrames = zeros(video.Height, video.Width, video.BitsPerPixel/8, requiredNumberOfFrames, 'uint8');
-        frameInterval = ( totalFrames - rem(totalFrames, requiredNumberOfFrames) ) / requiredNumberOfFrames;
-        i = 1;
-        while i <= requiredNumberOfFrames
-            for j = 1 : frameInterval - 1
+    i = 1;
+    while i <= requiredNumberOfFrames
+        for j = 1 : frameInterval - 1
 %                 skip (frameInterval - 1) frames
-                readFrame(video);
-            end
-            keyFrames(:,:,:,i) = readFrame(video);
-            i = i + 1;
+            readFrame(video);
         end
+        keyFrames(:,:,:,i) = readFrame(video);
+        i = i + 1;
     end
 
 end
