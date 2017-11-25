c7a47b784c422a59091cfdd5bef18283434b0a6f
Abhimanyu Singh Gaur <abhimanyusinghgaur@gmail.com>
2017-11-25 15:22:53 +0530

    Added test script

---
+++ CBVR_test.m
@@ -0,0 +1,9 @@
+testDataDir = 'TestData';
+testFiles = dir(sprintf('%s/*.*4', testDataDir));
+numFiles = length(testFiles);
+
+for j = 1 : numFiles
+    filename = sprintf('%s/%s', testDataDir, testFiles(j).name);
+%     fprintf('Filename: %s\n',filename);
+    CBVR;
+end
